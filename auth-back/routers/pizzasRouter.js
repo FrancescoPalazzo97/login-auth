2cb348988b74bd8d6789453ff76f17a75ce65068
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-09-14 12:24:00 +0200

    feat: update pizzas route to return data from JSON file

--- auth-back/routers/pizzasRouter.js
+++ auth-back/routers/pizzasRouter.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import fs from 'fs';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Lista pizze pazze!')
+    const pizzasString = fs.readFileSync('./data/pizzas.json', 'utf-8');
+    const pizzas = JSON.parse(pizzasString);
+    res.send({
+        title: 'Lista pizze pazze!',
+        data: pizzas
+    })
 })
 
 export default router;
